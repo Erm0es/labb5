756b234feb4556335f2551fd508a94446a8fc26c
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2022-09-21 12:15:35 +0200

    cont work.

--- src/main/java/edu/lernia/labb5/BoardGameMaterial.java
+++ src/main/java/edu/lernia/labb5/BoardGameMaterial.java
@@ -1,5 +1,67 @@
 package edu.lernia.labb5;
+import java.util.Scanner;
 
 public class BoardGameMaterial {
-    /* Will be used to store all boardgamematerials in one class */
+    
+    public static Dice[] dice;
+    private static boolean bGameIsOn = true;
+    private static boolean yatzi = true;
+
+    public static void StartGame() {
+        dice = new Dice[5];
+        for (int i = 0; i < 5; i++) {
+            dice[i] = new Dice();
+        }
+    }
+
+    public static void ContinueGame() {
+        while (bGameIsOn == true) {
+            int turn = 0;
+            System.out.println("Welcome to Yatzi!");
+            while (turn < 3) {
+                System.out.println("Starting turn " + (turn + 1) + " of 3, rolling dice.");
+                for (int roll = 0; roll < dice.length; roll++) {
+                    dice[roll].DiceRoll();
+                    // dice[roll].value = 5; //Test if yatzi work
+                    System.out.println(roll + ": " + dice[roll].getString());
+                }
+
+                CheckYatzi();
+
+                if (turn != 2) {
+                    System.out.println("Want to throw again? (y for yes, anything else for no)");
+                    Scanner inputScanner = new Scanner(System.in);
+                    if (inputScanner.next().equals("y")) {
+                        ++turn;
+                    } else {
+                        bGameIsOn = !bGameIsOn;
+                        break;
+                    }
+                } else {
+                    System.out.println("Game over! Want to play again?");
+                    Scanner scannerPlayAgain = new Scanner(System.in);
+                    if (scannerPlayAgain.next().equals("y")) {
+                        turn = 0;
+                    } else {
+                        bGameIsOn = !bGameIsOn;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    public static void CheckYatzi() {
+        for (int sameNumber = 1; sameNumber < 5; sameNumber++) {
+            if (dice[sameNumber].value != dice[sameNumber - 1].value) {
+                yatzi = false;
+            }
+            if (yatzi == true) {
+                System.out.println("You got YATZI! in " + dice[0].value + "'s");
+                return;
+            }
+        }
+    }
+   
+
 }
--- src/main/java/edu/lernia/labb5/YatziMain.java
+++ src/main/java/edu/lernia/labb5/YatziMain.java
@@ -1,74 +1,51 @@
 package edu.lernia.labb5;
 
-import java.util.Scanner;
-
-public class YatziMain {
-    public static Dice[] dice;
-    public static boolean bGameIsOn = true;
-    static boolean yatzi = true;
-
+public class YatziMain extends BoardGameMaterial{
     public static void main(String[] args) {
         StartGame();
         ContinueGame();
     }
 
-    public static void StartGame() {
-        dice = new Dice[5];
-        for (int i = 0; i < 5; i++) {
-            dice[i] = new Dice();
-        }
-    }
 
-    public static void CheckYatzi(boolean yatzi) {
-        for (int i = 1; i < 5; i++) {
-            if (dice[i].value != dice[i - 1].value) {
-                yatzi = false;
-            }
-            if (yatzi == true) {
-                System.out.println("You got YATZI! in " + dice[0].value + "'s");
-                return;
-            }
-        }
-    }
 
-    public static void ContinueGame() {
-        while (bGameIsOn == true) {
-            int turn = 0;
-            System.out.println("Welcome to Yatzi!");
-            while (turn < 3) {
-                System.out.println("Starting turn " + (turn + 1) + " of 3, rolling dice.");
-                for (int roll = 0; roll < dice.length; roll++) {
-                    dice[roll].DiceRoll();
-                    // dice[roll].value = 5; //Test if yatzi work
-                    System.out.println(roll + ": " + dice[roll].getString());
-                }
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
 
-                CheckYatzi(yatzi);
+   
 
-                if (turn != 2) {
-                    System.out.println("Want to throw again? (y for yes, anything else for no)");
-                    Scanner inputScanner = new Scanner(System.in);
-                    if (inputScanner.next().equals("y")) {
-                        ++turn;
-                    } else {
-                        bGameIsOn = !bGameIsOn;
-                        break;
-                    }
+   
+   
+   
+   
+   
+   
+   
+   
+   
 
-                } else {
-                    System.out.println("Game over! Want to play again?");
-                    Scanner scannerPlayAgain = new Scanner(System.in);
-                    if (scannerPlayAgain.next().equals("y")) {
-                        turn = 0;
-                    } else {
-                        bGameIsOn = !bGameIsOn;
-                        break;
-                    }
+   
+   
+   
+   
+   
+   
+   
+   
+   
 
-                }
-            }
+   
+   
 
-        }
+   
 
-    }
+   
 }
